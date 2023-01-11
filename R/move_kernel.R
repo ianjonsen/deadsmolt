86c3d205b8eef42a2ec3b9447b38610f3447720f
Ian Jonsen <ian.jonsen@mq.edu.au>
2023-01-11 11:27:55 -0400

    tweaks to move_kernel

--- R/move_kernel.R
+++ R/move_kernel.R
@@ -12,19 +12,32 @@ move_kernel <- function(data, xy = NULL, mpar, i, s, ts, d2l, dir2l) {
     if (d2l > mpar$pars$buffer) {
       switch(mpar$scenario,
              sobi = {
-                 ## state 1: migration toward SoBI
+                 ## state 1: migration biased toward SoBI
                  delta <- c(mpar$pars$coa[1] - xy[1], mpar$pars$coa[2] - xy[2])
 
                  mu <- atan2(delta[1], delta[2])
+
+                 ## strength of bias to CoA is a hyberbolic fn of distance to CoA
                  #rho <- 1 + tanh(mpar$pars$r * sqrt(sum(delta^2)))
+
+                 ## fixed rho gives strength of bias to the CoA
                  phi <- rwrpcauchy(1, mu, mpar$pars$rho[1])
-                 ## state 2: stop migration and remain in current water mass
+
+
+                 ## state 2: smolt outside of preferred T range
                  ## NEED TO REVISIT THIS - smolts should slow down/reverse when < ts.min,
                  ##   speed up when > ts.max to 'catch up' to preferred water mass
-                 if (ts < mpar$pars$tsr[1]) s <- s * 1
+                 if (ts < mpar$pars$tsr[1]) {
+                   ## slow down & head southward (presumably to warmer water mass)
+                   phi <- runif(1, 120, 240) / 180*pi
+                   s <- s * 0.5
+                 }
+                 ## speed up heading northward (presumably to colder water mass)
                  if (ts > mpar$pars$tsr[2]) s <- s * 1.5
              })
+
     } else {
+      ## if close to land (within mpar$pars$buffer km but farther than 2km)
         if(d2l > 2) {
           if(xy[2] < 1475) {
             ## direct kelt to move directly toward CoA (N end of SoBI) if in
